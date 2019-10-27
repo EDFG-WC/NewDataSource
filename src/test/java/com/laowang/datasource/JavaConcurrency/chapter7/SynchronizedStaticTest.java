c81e1a8e8ab97a4f0939170260f469d28676a6cb
EDFG-WC <wangchen706@hotmail.com>
2019-10-27 12:54:24 +0800

    线程练习题

---
+++ src/test/java/com/laowang/datasource/JavaConcurrency/chapter7/SynchronizedStaticTest.java
@@ -0,0 +1,26 @@
+package com.laowang.datasource.JavaConcurrency.chapter7;
+
+public class SynchronizedStaticTest {
+    public static void main(String[] args) {
+        new Thread("T1"){
+            @Override
+            public void run() {
+                SynchronizedStatic.method1();
+            }
+        }.start();
+
+        new Thread("T2"){
+            @Override
+            public void run() {
+                SynchronizedStatic.method2();
+            }
+        }.start();
+
+        new Thread("T3"){
+            @Override
+            public void run() {
+                SynchronizedStatic.method3();
+            }
+        }.start();
+    }
+}
