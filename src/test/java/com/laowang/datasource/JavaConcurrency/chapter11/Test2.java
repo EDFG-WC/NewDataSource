a146c30c21431b1a46b1f936ad74d4b7d6120510
EDFG-WC <wangchen706@hotmail.com>
2019-11-10 14:41:40 +0800

    线程练习题

---
+++ src/test/java/com/laowang/datasource/JavaConcurrency/chapter11/Test2.java
@@ -0,0 +1,12 @@
+package com.laowang.datasource.JavaConcurrency.chapter11;
+
+        import java.util.Arrays;
+        import java.util.Optional;
+
+public class Test2 {
+    public void test() {
+        Arrays.asList(Thread.currentThread().getStackTrace()
+        ).stream().filter(e -> !e.isNativeMethod())
+                .forEach(e -> Optional.of(e.getClassName() + ": " + e.getMethodName() + ": " + e.getLineNumber()).ifPresent(System.out::println));
+    }
+}
