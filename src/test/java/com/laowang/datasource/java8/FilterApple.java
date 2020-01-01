f0999691a2223ae8e5588f09a8e2074b255e0d91
EDFG-WC <wangchen706@hotmail.com>
2020-01-01 10:40:24 +0800

    线程练习题

--- src/test/java/com/laowang/datasource/java8/FilterApple.java
+++ src/test/java/com/laowang/datasource/java8/FilterApple.java
@@ -7,9 +7,19 @@ import java.util.List;
 public class FilterApple {
 
   // 过滤的handler模式
+
+  /**
+   * 1 该注解只能标记在"有且仅有一个抽象方法"的接口上。 2 JDK8接口中的静态方法和默认方法，都不算是抽象方法。 3 接口默认继承java.lang.Object，所以如果接口显示声明覆盖了Object中方法，那么也不算抽象方法。
+   * 4 该注解不是必须的，如果一个接口符合"函数式接口"定义，那么加不加该注解都没有影响。加上该注解能够更好地让编译器进行检查。如果编写的不是函数式接口，但是加上了@FunctionInterface，那么编译器会报错。
+   */
+  @FunctionalInterface
   public static interface AppleFilter {
 
     boolean filter(Apple apple);
+
+    default void print(String var) {
+      System.out.println(var);
+    }
   }
 
   public static class GreenAndHigherThan160Filter implements AppleFilter {
@@ -61,16 +71,17 @@ public class FilterApple {
     return list;
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
     List<Apple> list = Arrays.asList(Apple.builder().color("green").weight(150).build(),
         Apple.builder().color("yellow").weight(120).build(),
         Apple.builder().color("green").weight(170).build());
-//    List<Apple> greenApples = findGreenApple(list);
-//    assert greenApples.size() == 3 : "没有2个苹果";
-//    System.out.println(greenApples);
-    List<Apple> result = findApple(list, new GreenAndHigherThan160Filter());
-    System.out.println(result);
-    List<Apple> apple = findApple(list, new YellowAndLessThan150Filter());
-    System.out.println(apple);
+    List<Apple> lambdaResult = findApple(list, apple -> apple.getColor().equals("green"));
+    System.out.println(lambdaResult);
+
+    Thread test = new Thread(() -> {
+      System.out.println("你喜欢就好 " + Thread.currentThread().getName());
+    }, "test");
+    test.start();
+    Thread.currentThread().join();
   }
 }
--- src/test/java/com/laowang/datasource/javaconcurrency/phase2/chapter15/MessageHandler.java
+++ src/test/java/com/laowang/datasource/javaconcurrency/phase2/chapter15/MessageHandler.java
@@ -8,27 +8,15 @@ public class MessageHandler {
 
   private static final Random random = new Random(System.currentTimeMillis());
 
-  private final static Executor executor = Executors.newFixedThreadPool(5);
-
   public void request(Message message) {
-    executor.execute(() -> {
+    new Thread(() -> {
       String value = message.getValue();
       try {
         Thread.sleep(random.nextInt(1_000));
-        System.out.println(
-            "The message will be handled by " + Thread.currentThread().getName() + " " + value);
+        System.out.println("The message will be handled by " + Thread.currentThread().getName());
       } catch (InterruptedException e) {
         e.printStackTrace();
       }
-    });
-//    new Thread(() -> {
-//      String value = message.getValue();
-//      try {
-//        Thread.sleep(random.nextInt(1_000));
-//        System.out.println("The message will be handled by " + Thread.currentThread().getName());
-//      } catch (InterruptedException e) {
-//        e.printStackTrace();
-//      }
-//    }).start();
+    }).start();
   }
 }
