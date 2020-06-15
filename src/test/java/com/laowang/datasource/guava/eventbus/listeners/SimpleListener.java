91f9cb6d3c142964f234aae118155c510b189828
alex <wangchen706@hotmail.com>
2020-06-16 00:32:55 +0800

    6.16commit

--- src/test/java/com/laowang/datasource/guava/eventbus/listeners/SimpleListener.java
+++ src/test/java/com/laowang/datasource/guava/eventbus/listeners/SimpleListener.java
@@ -5,14 +5,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SimpleListener {
+    // 日志
+    private static final Logger LOGGER = LoggerFactory.getLogger(SimpleListener.class);
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(SimpleListener.class);
+    @Subscribe // guava专用注解, 用来监听/订阅消息
+    public void doAction(final String event) {
 
-  @Subscribe
-  public void doAction(final String event) {
-
-    if (LOGGER.isInfoEnabled()) {
-      LOGGER.info("Received event [{}] and will take a action", event);
+        if (LOGGER.isInfoEnabled()) {
+            LOGGER.info("Received event [{}] and will take a action", event);
+        }
     }
-  }
 }
